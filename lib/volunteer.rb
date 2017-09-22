ead938b964eb15e67a862c06093bf52356b5b3e7
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-22 09:39:26 -0700

    pass project_id test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,8 +1,9 @@
 class Volunteer
-  attr_accessor(:name)
+  attr_accessor(:name, :project_id)
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
   end
 
 end
