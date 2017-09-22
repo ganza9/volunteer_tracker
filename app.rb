372048612dffe97339ce9680079c281109112a94
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-22 16:35:08 -0700

    final commit

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require("./lib/volunteer")
 require("./lib/project")
 require("pg")
 
-# DB = PG.connect({:dbname => "volunteer_tacker"})
+DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/')do
   erb(:index)
