e3700c7c1db059334884f2e37954877660ade92f
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-10-01 20:01:09 -0700

    edit naming and routing

--- app.rb
+++ app.rb
@@ -12,16 +12,17 @@ get('/')do
 end
 
 get('/volunteers')do
-  @volunteers = Volunteers.all()
+  @volunteers = Volunteer.all()
   erb(:volunteers)
 end
 
 get('/volunteers/new')do
-  erb(:volunteers_form)
+  erb(:volunteer_form)
 end
 
 get('/volunteer/:id')do
   @volunteer = Volunteer.find(params.fetch(:id).to_i)
+  erb(:volunteer_detail)
 end
 
 post('/volunteers')do
@@ -31,11 +32,38 @@ post('/volunteers')do
   erb(:success)
 end
 
+get('/projects')do
+  @projects = Projects.all()
+  erb(:projects)
+end
+
+get('/projects/new')do
+  erb(:project_form)
+end
+
+get('/projects/:id')do
+  @project = Project.find(params.fetch('id').to_i())
+  erb(:project_detail)
+end
+
+delete("/projects/:id") do
+  @project = Project.find(params["id"].to_i)
+  @project.delete
+  @projects = Project.all
+  redirect '/projects'
+end
+
 post('/projects')do
-  name = params.fetch("name")
+  title = params.fetch("title")
   volunteer_id = params.fetch("volunteer_id").to_i()
   @volunteer = Volunteer.find(volunteer_id)
-  @project = Project.new({:name => name, :project_id => project_id, :id => nil})
+  @project = Project.new({:title => title, :project_id => project_id, :id => nil})
   @project.save()
   erb(:success)
+end
+
+get("/project/edit/:id") do
+  @project = Project.find(params['id'].to_i)
+  @projects = Project.all
+  erb(:project_edit)
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,41 +1,62 @@
 class Project
-  attr_accessor(:name, :volunteer_id, :id)
+  attr_accessor(:title, :id)
 
   def initialize(attributes)
-    @name = attributes.fetch(:name)
-    @volunteer_id = attributes.fetch(:volunteer_id)
+    @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
 
   def ==(another_project)
-    self.name().==(another_project.name()).&(self.volunteer_id().==(another_project.volunteer_id()))
+    self.title().==(another_project.title()).&(self.id().==(another_project.id()))
   end
 
   def self.all
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
     returned_projects.each() do |project|
-      name = project.fetch('name')
-      volunteer_id = project.fetch('volunteer_id')
+      title = project.fetch('title')
       id = project.fetch('id').to_i
-      attributes = {:name => name, :volunteer_id => volunteer_id, :id => id}
-      projects.push(project.new(attributes))
+      attributes = {:title => title, :id => id}
+      projects.push(Project.new(attributes))
     end
     projects
   end
 
   def save
-    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch('id').to_i()
   end
 
   def self.find
     found_project = nil
-    project.all().each() do |project|
+    Project.all().each() do |project|
       if project.id().==(id)
         found_project = project
       end
     end
     found_project
+  end
+
+  def update(attributes)
+    @title = attributes.fetch(:title, @title)
+    @id = self.id()
+    result = DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = '#{@id}' ;")
+  end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id()};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{self.id()};")
+  end
+
+  def volunteers
+    project_volunteers = []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = '#{self.id()}';")
+    volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      project_volunteers.push(Volunteer.new({name: name, project_id: project_id, id: id}))
+    end
+    project_volunteers
   end
 end
